176ed8aab24cd3af41b870313a494912c4ac8934
Willis Plummer <willisplummer@gmail.com>
2023-09-21 06:50:53 -0600

    format

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -19,7 +19,7 @@ local rust_tools = require('rust-tools')
 rust_tools.setup({
   server = {
     on_attach = function(_, bufnr)
-      vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, {buffer = bufnr})
+      vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
     end,
     settings = {
       ["rust-analyzer"] = {
