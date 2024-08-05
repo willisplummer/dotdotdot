aab8d47332fd3600a5f95d83ff3b7bfed35a2e22
Willis Plummer <willisplummer@gmail.com>
2024-08-05 10:29:26 -0600

    enable natural editing in wezterm -- note this might be more of a zsh thing though

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -16,5 +16,14 @@ config.color_scheme = "Tokyo Night"
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
+config.keys = {
+  { mods = "OPT", key = "LeftArrow",  action = wezterm.action.SendKey({ mods = "ALT", key = "b" }) },
+  { mods = "OPT", key = "RightArrow", action = wezterm.action.SendKey({ mods = "ALT", key = "f" }) },
+  { mods = "CMD", key = "LeftArrow",  action = wezterm.action.SendKey({ mods = "CTRL", key = "a" }) },
+  { mods = "CMD", key = "RightArrow", action = wezterm.action.SendKey({ mods = "CTRL", key = "e" }) },
+  { mods = "CMD", key = "Backspace",  action = wezterm.action.SendKey({ mods = "CTRL", key = "u" }) },
+
+}
+
 -- and finally, return the configuration to wezterm
 return config
